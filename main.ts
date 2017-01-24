c5d81d7185eb3cbed56671613072e50a120de1d5
Jonathan Kupcho <jonathan.kupcho@gmail.com>
2017-01-24 14:15:04 -0600

    array observable

--- main.ts
+++ main.ts
@@ -1,49 +1,16 @@
 import { Observable } from "rxjs";
-import { loadWithFetch, load } from './loader';
 
-// let source = Observable.merge(
-//   Observable.of(1),
-//   Observable.from([2,3,4]),
-//   Observable.throw(new Error("Stop!")),
-//   Observable.of(5)
-// // Allows for returning an Observable if an error occurs.
-// ).catch(e => {
-//   console.log(`caught: ${e}`);
-//   return Observable.of(10);
-// });
+let output = document.getElementById("content");
 
-// source.subscribe(
-//   value => console.log(`value ${value}`),
-//   error => console.error(`error: ${error}`),
-//   () => console.log("complete")
-// );
+let arrayObservable = Observable.from([1, 2, 3, 'boo']);
 
-let output = document.getElementById("output");
-let button = document.getElementById("button");
-
-let click = Observable.fromEvent(button, "click");
-
-
-let subscription = load("moviess.json")
-  .subscribe(renderMovies,
-    e => console.log(`error: ${e}`),
-    () => console.log("complete!")
-);
-
-subscription.unsubscribe();
-
-function renderMovies(movies) {
-  movies.forEach(m => {
-    let div = document.createElement("div");
-    div.innerText = m.title;
-
-    output.appendChild(div);
-  })
-}
-
-click.flatMap(e => loadWithFetch("movies.json"))
-  .subscribe(
-  renderMovies,
-  e => console.error(`error: ${e}`),
-  () => console.log("complete")
-);
+arrayObservable.subscribe(
+  value => {
+    let element = document.createElement("span");
+    // convert to string
+    element.innerText = '' + value;
+    output.appendChild(element);
+  },
+  err => console.error(`An error occurred: ${err}`),
+  () => console.log('Completed!')
+)

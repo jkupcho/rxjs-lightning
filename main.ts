03b3e6edf4bae2977193eae4fd811e570526db53
Jonathan Kupcho <jonathan.kupcho@gmail.com>
2017-01-24 15:20:16 -0600

    added multiple subscribers

--- main.ts
+++ main.ts
@@ -1,8 +1,11 @@
 import { Observable } from "rxjs";
 
 let output = document.getElementById("content");
+let scaryOutput = document.getElementById("scary-words");
 
-let arrayObservable = Observable.from([1, 2, 3, 'boo'])
+let arrayObservable = Observable.from([1, 2, 3, 'boo', 'RAWR!']);
+
+let listObservable = arrayObservable
   .filter(value => !isNaN(+value))
   .map(value => `Number: ${value}`)
   .scan((acc, value) => {
@@ -12,10 +15,24 @@ let arrayObservable = Observable.from([1, 2, 3, 'boo'])
     return acc;
   }, document.createElement("ol"));
 
-arrayObservable.subscribe(
+let stringObservable = arrayObservable
+  .filter(value => isNaN(+value))
+  .map(value => `${value} is a scary word!`)
+  .scan((acc, value) => {
+    const paragraph = document.createElement("p");
+    paragraph.innerText = value;
+    acc.appendChild(paragraph);
+    return acc;
+  }, document.createElement("div"));
+
+listObservable.subscribe(
   value => {
     output.appendChild(value);
   },
   err => console.error(`An error occurred: ${err}`),
   () => console.log('Completed!')
-)
+);
+
+stringObservable.subscribe(
+  value => scaryOutput.appendChild(value)
+);

f6202006ff32a91601a7c78cf45c89bb44eb19aa
Jonathan Kupcho <jonathan.kupcho@gmail.com>
2017-01-24 15:12:19 -0600

    aggregate list

--- main.ts
+++ main.ts
@@ -4,14 +4,17 @@ let output = document.getElementById("content");
 
 let arrayObservable = Observable.from([1, 2, 3, 'boo'])
   .filter(value => !isNaN(+value))
-  .map(value => `Number: ${value}`);
+  .map(value => `Number: ${value}`)
+  .scan((acc, value) => {
+    const li = document.createElement("li");
+    li.innerText = value;
+    acc.appendChild(li);
+    return acc;
+  }, document.createElement("ol"));
 
 arrayObservable.subscribe(
   value => {
-    let element = document.createElement("span");
-    // convert to string
-    element.innerText = '' + value;
-    output.appendChild(element);
+    output.appendChild(value);
   },
   err => console.error(`An error occurred: ${err}`),
   () => console.log('Completed!')
